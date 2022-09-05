712c77a88f79e9419ec59e44d4fd2c63e2e2db63
NTCNTS <sylinh412@gmail.com>
2022-09-05 10:53:35 +0700

    abc

---
+++ app/src/main/java/com/example/tuan3_tltdd/ChildActivity.java
@@ -0,0 +1,26 @@
+package com.example.tuan3_tltdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+
+public class ChildActivity extends AppCompatActivity {
+    Button btnChild;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        btnChild = (Button) findViewById(R.id.buttonChild);
+        btnChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
